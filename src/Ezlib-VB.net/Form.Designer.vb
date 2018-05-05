477ffc022a7b534389e62b16ada58eae7d75babb
zarlo5899@gmail.com <zarlo5899@gmail.com>
2018-05-05 23:33:10 +1000

    update

--- src/Ezlib-VB.net/Form.Designer.vb
+++ src/Ezlib-VB.net/Form.Designer.vb
@@ -22,9 +22,16 @@ Partial Class Form
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "Form"
         Me.Text = "Form"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
